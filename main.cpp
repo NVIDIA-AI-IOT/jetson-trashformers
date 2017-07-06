be957380bfd8447cdcfe36250b7526499a8e8fae
Ishan Mitra <ishanbubby@gmail.com>
2017-07-06 11:33:35 -0700

    Add exit handler for main loop

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include "include/Humanoid.h"
 #include <iostream>
+#include <signal.h>
+
+sig_atomic_t signaled = 0;
+
+void handler(int signum) {
+    signaled = 1;
+}
+
 
 int main (int argc, char** argv){
     Humanoid* humanoid = new Humanoid(argc, argv);
     
+    signal(SIGINT, handler);
     float** bbArray;
     volatile int numBB;
     std::cin.ignore(); 
-    while(true){
+     
+    while(!signaled){
         bbArray = humanoid->detectnetController->GetBBArray();
         numBB = *humanoid->detectnetController->GetNumBB();
         printf("numBB %i\n:", numBB);
         for(int i=0; i< numBB; i++){
             float* bb = bbArray[i];
-            for(int j=0; j<4; j++) {
-                printf("BB %i: %f, %f, %f, %f\n", i, bb[0], bb[1], bb[2], bb[3]);
-            }
+            printf("BB %i: %f, %f, %f, %f\n", i, bb[0], bb[1], bb[2], bb[3]);
         }
         std::cin.ignore();
     }
