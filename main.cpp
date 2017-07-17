67363e2e2c48c56ad373d01be063e8e80c90e79e
Michael Chacko <mikechacko10@gmail.com>
2017-07-17 17:37:54 +0000

    reword and/or remove print statements

--- main.cpp
+++ main.cpp
@@ -29,18 +29,16 @@ int main (int argc, char** argv){
     //while((inputChar = getchar()) != 27){
     while(!humanoid->detectnetController->ReadStopSignal()){
         humanoid->detectnetController->SortBBArrayByTargetDistance();
-        printf("Orientation: %i\n", humanoid->detectnetController->GetCupOrientation());
         
         float xError = humanoid->detectnetController->GetErrorXOfTargetBB();
         float bbArea = humanoid->detectnetController->GetAreaOfTargetBB(); 
 
-        printf("AREA: %f\n", bbArea);
         if(xError == NULL || bbArea == -1) {
             if(bendDown){
                 humanoid->behaviorController->ChangeState(BehaviorController::ControllerState::WALK_FORWARD);
                 humanoid->behaviorController->ChangeState(BehaviorController::ControllerState::STOP);
                 humanoid->behaviorController->ChangeState(BehaviorController::ControllerState::STRAFE_LEFT);
-                printf("XERROR DNE | BEND DOWN\n"); 
+                printf("BEND DOWN\n"); 
                 sleep(1);
                 humanoid->arm->SetPose(Arm::ArmPose::READY);
                 sleep(1);
@@ -50,35 +48,31 @@ int main (int argc, char** argv){
                 break;
             }
             else {
-               printf("XERROR DNE | STOP\n"); 
+               printf("STOP\n"); 
                humanoid->behaviorController->ChangeState(BehaviorController::ControllerState::STOP);
             }
         } else if(xError >= xReactionTolerance) {
-            printf("YERROR: %f | TURNING RIGHT\n", xError);
+            printf("TURNING RIGHT\n", xError);
             humanoid->behaviorController->ChangeState(BehaviorController::ControllerState::STRAFE_RIGHT);
         } else if(xError <= (xReactionTolerance)*-1) {
-            printf("XERROR: %f | TURNING LEFT\n", xError);
+            printf("TURNING LEFT\n", xError);
             humanoid->behaviorController->ChangeState(BehaviorController::ControllerState::STRAFE_LEFT);
         } else if(bbArea <= areaTolerance){
-            printf("ERROR: %f | WALKING FORWARD\n", bbArea);
+            printf("WALKING FORWARD\n", bbArea);
             humanoid->behaviorController->ChangeState(BehaviorController::ControllerState::WALK_FORWARD);
             humanoid->behaviorController->ChangeState(BehaviorController::ControllerState::STOP);
         } else {
-            printf("ERROR: %f | STOP DUE TO LARGE AREA\n", xError);
+            printf("STOP DUE TO LARGE AREA\n", xError);
             humanoid->behaviorController->ChangeState(BehaviorController::ControllerState::STOP);
         } 
 
         if(humanoid->detectnetController->bbArraySorted.size() < 1){
             bendDown = false; 
-            printf("SETTING FALSE");
         }
         else if( humanoid->detectnetController->GetCenterYFromBB(humanoid->detectnetController->bbArraySorted[0]) > ((2.0/3.0) * humanoid->detectnetController->GetCameraHeight()) ){
             bendDown = true; 
-            printf("SETTING TRUE");
-            printf("CENTER Y of BB: %f\n", humanoid->detectnetController->GetCenterYFromBB(humanoid->detectnetController->bbArraySorted[0]) );
-            printf("image threshold: %f\n", ((2.0/3.0) * humanoid->detectnetController->GetCameraHeight()) );
-        }
-        else {
+            //printf("CENTER Y of BB: %f\n", humanoid->detectnetController->GetCenterYFromBB(humanoid->detectnetController->bbArraySorted[0]) );
+            //printf("image threshold: %f\n", ((2.0/3.0) * humanoid->detectnetController->GetCameraHeight()) );
         }
 
         sleep(1);
